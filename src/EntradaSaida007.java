b7378f7535f788b95f13f2d959fbc9cbbf498c9f
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-12-21 00:37:13 -0300

    exercicio de entrada e saida 7 terminada

---
+++ src/EntradaSaida007.java
@@ -0,0 +1,24 @@
+//Faça um pseudocódigo que peça ao usuário um número inteiro. A partir
+//dessa entrada, mostre na tela: o número, seu antecessor e seu sucessor. Ex.:
+//“O número informado foi 30, o antecessor é 29 e o sucessor é 31”.
+import java.util.Scanner;
+
+public class EntradaSaida007 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        //declaracao de variaveis
+        int numero, numeroAntecessor, numeroSucessor = 0;
+        System.out.println("Digite um numero: ");
+        numero = ler.nextByte();
+
+        numeroAntecessor = numero-1;
+        numeroSucessor = numero+1;
+
+        System.out.println("O numero digitado foi: " + numero);
+        System.out.println("O numero sucessor foi: " + numeroSucessor);
+        System.out.println("O numero antecessor foi: " + numeroAntecessor);
+
+        ler.close();
+    }
+}
